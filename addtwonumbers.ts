b63b68b8a8fb95f9cb28f2a83b8a1fd74ac66ca3
Nadeen shaik <167284823+ShaikNadeen@users.noreply.github.com>
2024-08-16 11:03:17 +0530

    testing message 1

--- addtwonumbers.ts
+++ addtwonumbers.ts
@@ -1,11 +1,4 @@
 
-/**
- * Adds two numbers together.
- *
- * @param {number} a - The first number.
- * @param {number} b - The second number.
- * @returns {number} The sum of the two numbers.
- */
 function addNumbers(a, b) {
   return a + b;
 }
