2f34ebcf5558867d37d0e75d318cacd82bb04a14
appmod-ai <appmod@techolution.com>
2024-10-09 11:56:30 +0000

    Cleaned directory in media-dimension-calculation-width-and-height-calculation-2b9372

--- array.service.ts
+++
@@ -1,3 +0,0 @@
-export function removeDuplicateElementsFromArray<ArrayElementType>(array: Array<ArrayElementType>): Array<ArrayElementType> {
-	return array.filter((item, index) => array.indexOf(item) === index);
-}
